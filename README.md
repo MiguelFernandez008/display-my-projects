feca2945fcdbc3916705a8e34687cb70996727ab
Miguel Fernández <miguel.fernandez008@outlook.es>
2021-02-07 18:22:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# display-my-projects
+ A plugin to manage projects and tasks
---
+++ display-my-projects.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name:       Display my projects
+ * Plugin URI:        https://displaytable.es
+ * Description:       A plugin to manage projects and tasks.
+ * Version:           0.1
+ * Requires at least: 5.6
+ * Requires PHP:      7.2
+ * Author:            Display Table
+ * Author URI:        https://displaytable.es
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       display-my-projects
+ * Domain Path:       /languages
+ */
+
+defined('ABSPATH') or die();
